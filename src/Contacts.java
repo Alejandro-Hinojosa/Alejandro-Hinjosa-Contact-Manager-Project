545a0c80924ab7ce0ede16dc20b9a23d83f3cad4
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-11-09 12:29:54 -0600

    added two contacts

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,5 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Contacts {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("1. View contacts.");
         System.out.println("2. Add a new contact.");
@@ -7,5 +16,36 @@ public class Contacts {
         System.out.println("4. Delete an existing contact.");
         System.out.println("5. Exit.");
         System.out.println("Enter an option (1, 2, 3, 4, or 5):");
+
+
+
+        //Let's make some new stuff - CREATING files and directories in JAVA
+        String directory = "./src/data";
+        String filename = "contacts.txt";
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+        //building the whole shebang! A path combining: the directory's path + the desired filename at the end :D
+
+        System.out.println(dataFile);
+
+        if(Files.notExists(dataFile)){
+            Files.createFile(dataFile);
+        }
+
+        Path contactPath = Paths.get(directory, filename);
+
+        System.out.println("contactPath = " + contactPath);
+        System.out.println("Files.exists(contactPath) = " + Files.exists(contactPath));
+
+        List<String> contactList = Arrays.asList("John - 21021212122", "Matthias - 9090227474");
+        System.out.println("contactList = " + contactList);
+
+        Files.write(contactPath, contactList);
+        System.out.println();
+        //Reading from the file
+        List<String> printList = Files.readAllLines(contactPath);
+        System.out.println("printList = " + printList);
+
+
     }
 }
